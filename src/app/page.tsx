745f45dc24c15829c3d85f25d9d97fad881c903a
uday <udaysah0003@gmail.com>
2024-04-15 01:30:44 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,173 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [cities, setCities] = useState<any[]>([]);
+  const [fetchedCities, setFetchedCities] = useState<any[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [sortBy, setSortBy] = useState<string>("");
+
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=${
+          page * 20
+        }`
+      );
+      const newCities = response.data.records.map((record: any) => ({
+        cityName: record.name,
+        countryName: record.cou_name_en,
+        timezone: record.timezone,
+        coordinates: record.coordinates,
+      }));
+      setFetchedCities(newCities);
+      const mergedCities = [...cities, ...newCities];
+      setCities(mergedCities);
+      setPage(page + 1);
+      if (newCities.length === 0) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error("Error fetching city data:", error);
+    }
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+    // Filter cities based on search term locally
+    const filteredCities = fetchedCities.filter((city: any) =>
+      city.cityName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCities(filteredCities);
+  };
+
+  const handleCityClick = (cityName: string) => {
+    router.push(`/weather/${encodeURIComponent(cityName)}`);
+  };
+  const handleContextMenu = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const cityName = e.currentTarget.textContent || ""; // Provide a default value if cityName is null
+    window.open(`/weather/${encodeURIComponent(cityName)}`, "_blank");
+  };
+
+  const handleSort = (column: string) => {
+    const direction =
+      sortBy === column ? (sortDirection === "asc" ? "desc" : "asc") : "asc";
+    setSortBy(column);
+    setSortDirection(direction);
+    const sortedCities = [...cities].sort((a, b) => {
+      if (a[column] < b[column]) return direction === "asc" ? -1 : 1;
+      if (a[column] > b[column]) return direction === "asc" ? 1 : -1;
+      return 0;
+    });
+    setCities(sortedCities);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: 'url("/bgImg.jpg")' }}
+      >
+        <div className="absolute inset-0 bg-black opacity-70 blur"></div>
+      </div>
+      <div className="min-h-screen bg-gray-900 text-white relative z-10">
+        <div className="container mx-auto py-8">
+          <input
+            type="text"
+            placeholder="Search cities..."
+            value={searchTerm}
+            onChange={handleSearch}
+            className="w-full p-3 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring focus:border-blue-300"
+          />
+          <div className="mt-8">
+            <InfiniteScroll
+              dataLength={cities.length}
+              next={fetchData}
+              hasMore={hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={<p>No more cities to load</p>}
+            >
+              <table className="w-full">
+                <thead>
+                  <tr>
+                    <th
+                      className="px-4 py-2 cursor-pointer"
+                      onClick={() => handleSort("cityName")}
+                    >
+                      City Name{" "}
+                      {sortBy === "cityName" &&
+                        (sortDirection === "asc" ? "↑" : "↓")}
+                    </th>
+                    <th
+                      className="px-4 py-2 cursor-pointer"
+                      onClick={() => handleSort("countryName")}
+                    >
+                      Country{" "}
+                      {sortBy === "countryName" &&
+                        (sortDirection === "asc" ? "↑" : "↓")}
+                    </th>
+                    <th
+                      className="px-4 py-2 cursor-pointer"
+                      onClick={() => handleSort("timezone")}
+                    >
+                      Timezone{" "}
+                      {sortBy === "timezone" &&
+                        (sortDirection === "asc" ? "↑" : "↓")}
+                    </th>
+                    <th className="px-4 py-2">Map</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cities.map((city, index) => (
+                    <tr key={index}>
+                      <td className="border px-4 py-2">
+                        <Link
+                          href={`/weather/${encodeURIComponent(city.cityName)}`}
+                        >
+                          <a
+                            className="text-blue-500 hover:underline"
+                            onClick={() => handleCityClick(city.cityName)}
+                            onContextMenu={handleContextMenu}
+                          >
+                            {city.cityName}
+                          </a>
+                        </Link>
+                      </td>
+                      <td className="border px-4 py-2">{city.countryName}</td>
+                      <td className="border px-4 py-2">{city.timezone}</td>
+                      <td className="border px-4 py-2">
+                        <a
+                          href={`https://www.google.com/maps?q=${city.coordinates.lat},${city.coordinates.lon}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 hover:underline"
+                        >
+                          View Map
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </InfiniteScroll>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
